311e116a7ef6bb4dc8d739acc1224477220b59fc
Septahadif <110327728+Septahadif@users.noreply.github.com>
2025-11-29 16:33:30 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,28 +1,15 @@
-const target_host = 'vless.masjawa.my.id';
-const target_port = '10000';
-const target_path = '/ed=2048';
-
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-    const headers = new Headers(request.headers);
     
-    // Modify headers for WebSocket
-    headers.set('Host', target_host);
-    headers.set('X-Forwarded-For', request.headers.get('CF-Connecting-IP'));
-    headers.set('X-Real-IP', request.headers.get('CF-Connecting-IP'));
-    
-    // WebSocket upgrade
     if (request.headers.get('Upgrade') === 'websocket') {
-      const wsUrl = `https://${target_host}:${target_port}${target_path}`;
-      
-      return await fetch(wsUrl, {
-        headers: headers,
+      return await fetch(`http://202.155.91.127:10000/ed=2048`, {
+        headers: request.headers,
         method: request.method,
         body: request.body
       });
     }
     
-    return new Response('VLess WS Worker Active', { status: 200 });
+    return new Response('Xray Worker Ready');
   }
-};
+}
